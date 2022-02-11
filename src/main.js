380ad8eb2444d10a3b936bfac03a79350d147b4a
km6293 <alstn7616@naver.com>
2022-02-11 22:30:23 +0900

    pwa

--- src/main.js
+++ src/main.js
@@ -5,12 +5,15 @@ let emitter = mitt();
 let app = createApp(App)
 // 글로벌한 변수보관함임
 app.config.globalProperties.emitter = emitter;
+
 // axios를 글로벌 변수로 보관할때는
 // app.config.globalProperties.axios = axios;
 // 그러면 이제 vue 파일에서 import axios 해올 필요 없이 this.axios로 사용가능
 
 // vuex
 import store from './store.js'
+
+import './registerServiceWorker'
 
 // use -> 모든 컴포넌트들이 store 공유
 app.use(store).mount('#app')
